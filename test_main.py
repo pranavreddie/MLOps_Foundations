2863d36acda65659c281dec199455f5a7a41d206
pranavreddie <reddypranav50@gmail.com>
2025-01-25 23:29:55 +0530

    Updated the test_main

--- test_main.py
+++ test_main.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-from sklearn.metrics import accuracy_score
 from app import model  # Assuming model is loaded in your Flask app
 
 
@@ -21,17 +19,3 @@ def test_prediction():
 
     assert prediction is not None
     assert prediction[0] in ['Setosa', 'Versicolor', 'Virginica']
-
-
-# Test accuracy with known test set
-def test_accuracy():
-    # Load dataset
-    df = pd.read_csv('data/iris.csv')  # Update the path to your dataset
-    X = df.drop(columns=['variety'])
-    y = df['variety']
-
-    # Evaluate model accuracy on the test set
-    predictions = model.predict(X)
-    accuracy = accuracy_score(y, predictions)
-    # Assert that the accuracy is greater than or equal to 90%
-    assert accuracy >= 0.9
